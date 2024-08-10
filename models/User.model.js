4b84aafda255b78603fd8ed8fc9bac1db6152e27
Joshua George <horheyinc8@gmail.com>
2024-08-10 14:06:22 +0200

    first time

---
+++ models/User.model.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the User model to whatever makes sense in this case
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is required."],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required."],
+    },
+    name: String,
+    profileImage: {
+      type: String,
+      default:
+        "https://images.unsplash.com/photo-1489633908075-1c914e8ee5ea?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
